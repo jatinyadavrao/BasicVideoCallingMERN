f09d080cead1c1c79b144fa2e897133d36eafa50
Jatin Yadav <107779131+jatinyadavrao@users.noreply.github.com>
2024-06-14 22:30:15 +0530

    Update SocketContext.jsx

--- frontend/src/SocketContext.jsx
+++ frontend/src/SocketContext.jsx
@@ -1,25 +1,27 @@
-
 import React, { createContext, useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
 export const SocketContext = createContext();
 
-
 export const SocketProvider = ({ children }) => {
     const [socket, setSocket] = useState(null);
 
     useEffect(() => {
-        const newSocket = io('https://basic-video-calling-mern-webrtc-backend.vercel.app');
-        setSocket(newSocket);
-        console.log('newSocket')
+        const newSocket = io('https://basic-video-calling-mern-webrtc-backend.vercel.app', {
+            transports: ['websocket'], // Explicitly enable WebSocket transport
+        });
 
+        setSocket(newSocket);
+
+        // Clean up the socket connection when component unmounts
         return () => {
             newSocket.close();
         };
     }, []);
 
+    // Ensure that the SocketContext.Provider provides the socket value to its children
     return (
-        <SocketContext.Provider value={{socket}}>
+        <SocketContext.Provider value={{ socket }}>
             {children}
         </SocketContext.Provider>
     );
