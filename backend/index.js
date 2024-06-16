a5ae9aef2a8a0354aecd4c06d5d79efabe76b7be
Jatin Yadav <107779131+jatinyadavrao@users.noreply.github.com>
2024-06-16 17:29:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,20 @@
 const express = require('express');
-const app = express();
 const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
+
+const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: '*'
+        origin: 'https://basic-video-calling-mern-webrtc-frontend.vercel.app',
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['Content-Type'],
     }
 });
 
-app.use(cors()); // Remove the previous app.use(cors(...)) middleware
-app.get("/", (req, res) => res.send("Express on Vercel"));
-// Socket.IO events handling remains the same
+app.use(cors());
+
 io.on('connection', (socket) => {
     console.log('Client connected:', socket.id);
 
@@ -42,6 +44,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(process.env.PORT || 3000, () => {
+server.listen(3000, () => {
     console.log("Listening at port 3000");
 });
